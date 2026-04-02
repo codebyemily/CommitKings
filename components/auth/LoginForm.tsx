b71406d06f6b2c9868aef116ce049d89ddcab11e
Mohammad Ali Hamza <malihamza2006@gmail.com>
2026-04-02 16:46:37 -0700

    Added Signup Page for the App

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import Link from 'next/link'
+import { type FormEvent, useState } from 'react'
+
+export function LoginForm() {
+  const [emailOrUsername, setEmailOrUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <form className="auth-form" onSubmit={handleSubmit} noValidate>
+        <label className="sr-only" htmlFor="email-or-username">
+          Email or Username
+        </label>
+        <input
+          id="email-or-username"
+          name="emailOrUsername"
+          type="text"
+          autoComplete="username"
+          placeholder="Email or Username"
+          value={emailOrUsername}
+          onChange={(e) => setEmailOrUsername(e.target.value)}
+        />
+
+        <label className="sr-only" htmlFor="password">
+          Password
+        </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          autoComplete="current-password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit" className="auth-submit">
+          Log In
+        </button>
+      </form>
+
+      <button type="button" className="auth-link forgot">
+        Forgot Password?
+      </button>
+
+      <div className="auth-divider" role="separator" />
+
+      <p className="auth-footer">
+        Don&apos;t have an account?{' '}
+        <Link className="auth-link auth-link-inline" href="/signup">
+          Sign Up
+        </Link>
+      </p>
+    </>
+  )
+}
